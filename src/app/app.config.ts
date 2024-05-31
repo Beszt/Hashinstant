a333adc7b57b6c5d9f91feec3335a940a4718e8d
Maciej Obarzanek <maciej.obarzanek@gmail.com>
2024-05-31 21:39:16 +0200

    Little tweaks

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { NgxsModule } from '@ngxs/store';
 import { ImageState } from './shared/state/image.state';
 import { OpenAiService } from './shared/services/open-ai.service';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,7 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
     OpenAiService
   ],
 };
